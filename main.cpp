6b1a30ceb1dd6d61307af4ac021032f52375d217
ojflores <ojflores@puc.edu>
2017-03-01 02:52:48 +0000

    second

--- main.cpp
+++ main.cpp
@@ -3,25 +3,10 @@
 #define ITEMS 5
 using namespace std;
 
-struct Menu{
-	string food_name;
-	int price;
-};
-
-
 int main(){
 	int i;
 	string menu[ITEMS] = {"salad", "meatloaf", "nachos", "pizza", "burrito"};
 	int menu_price[ITEMS] = {10, 12, 8, 9, 10};
-	
-	cout << menu[1] << endl;
-	cout << menu_price[1] << endl;
-	
-	
-	
-	
-	
-	
 	
 	
 	
