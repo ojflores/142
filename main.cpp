8f8e28809eeb74ac4cf02e4078edf434a5e4c5ef
ojflores <ojflores@puc.edu>
2017-03-01 02:50:57 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#define ITEMS 5
+using namespace std;
+
+struct Menu{
+	string food_name;
+	int price;
+};
+
+
+int main(){
+	int i;
+	string menu[ITEMS] = {"salad", "meatloaf", "nachos", "pizza", "burrito"};
+	int menu_price[ITEMS] = {10, 12, 8, 9, 10};
+	
+	cout << menu[1] << endl;
+	cout << menu_price[1] << endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
